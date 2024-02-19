96d785635df875a30c0c20febb5947d87dc232f1
Kosta-777 <150023312+Kosta-777@users.noreply.github.com>
2024-02-19 16:30:17 +0100

    3

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,13 @@
 function calcAverageCalories(days) {
   
-  let sum = 0;
-  
- 
-  console.log(days);
-
+ let sum = 0;
 
 for (const key of days) {
-sum += key.calories / days.length;}
+  sum += key.calories; }
   
-  
-  return sum;
-
-
+  if (days.length === 0) { return 0; }
+  let calculate = sum / days.length;
+  return calculate;
 
 
 }
@@ -29,3 +24,18 @@ console.log(
   ])
 ); // 3180
 
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
--- js/task-3.js
+++ js/task-3.js
@@ -20,9 +20,10 @@ return (`${this.username} has ${this.playTime} active hours!`);
 
 };
 
-profile.getInfo();
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
 profile.changeUsername("Marco");
-console.log(profile.getInfo());
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
 profile.updatePlayTime(20);
-console.log(profile.getInfo()); 
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
